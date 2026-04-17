644f87c989fe7fba7b4e2d073a70917ab24d21b4
qqvvv <34702774+qqvvv@users.noreply.github.com>
2026-04-17 14:23:03 +0800

    try-catch-error-console-error

--- .eleventy.js
+++ .eleventy.js
@@ -711,6 +711,7 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  eleventyConfig.addPassthroughCopy("src/site/6");
   eleventyConfig.addPassthroughCopy("src/site/img");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
