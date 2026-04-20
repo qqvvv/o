453af773d315a390cdca548ea4a726d9e3421bee
qqvvv <34702774+qqvvv@users.noreply.github.com>
2026-04-20 21:03:43 +0800

    window-referLibrary_coreModule

---
+++ src/site/6/imgLoadedPlus.js
@@ -0,0 +1,21 @@
+(async () => {
+  const coreModule = await import("https://rra.pages.dev/6/referLibrary.js");
+  try {
+    window.referLibrary = coreModule.referLibrary;
+    await referLibrary({
+      "imagesLoaded": "https://esm.sh/imagesloaded@5.0.0",
+      0: "https://rra.pages.dev/6/imgLoader.js",
+    });
+    const imageUrls = [
+      'https://picsum.photos/200/140/?1',
+      'https://picsum.photos/210/140/?2',
+      'https://picsum.photos/220/140/?3',
+      'https://picsum.photos/230/140/?4',
+      'https://picsum.photos/240/140/?5',
+    ];
+    const container = ImageLoader.create(imageUrls);
+    document.body.appendChild(container);
+  } catch (error) {
+    console.error(error);
+  }
+})();
