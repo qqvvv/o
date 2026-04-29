ff53d13fcceb262284d1fb33fee9e09ca376dc95
qqvvv <34702774+qqvvv@users.noreply.github.com>
2026-04-29 14:09:58 +0800

    export_class-cmdExecutor-cstruc

--- src/site/6/vectorMenu/main_noFrom.js
+++ src/site/6/vectorMenu/main_noFrom.js
@@ -2,7 +2,16 @@
   const url = "https://rra.pages.dev/6/vectorMenu/menuUI_esm.js";
   try {
     const { initializeApp } = await import( url );
-    initializeApp ();
+    const data = `
+- 层一功能1
+- 层一折叠1
+  - 层二折叠1
+    - 层三功能1
+    - 层三功能2
+  - 层二功能1 #?<endCluster=true &simpleAnchor=alias
+- 层一功能2 #?<comment= #YggdrAML
+`;
+    initializeApp ( data );
   } catch (error) {
     console.error(error);
   }
