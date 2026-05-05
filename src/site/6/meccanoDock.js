cee2cd9c979f74d3d1e32c16a92b3800ac0c98b3
qqvvv <34702774+qqvvv@users.noreply.github.com>
2026-05-05 14:48:03 +0800

    meccanoD-026_behaviours_refer

--- src/site/6/meccanoDock.js
+++ src/site/6/meccanoDock.js
@@ -1,6 +1,6 @@
 /**
  * meccanoDock
- * 0.24
+ * 0.26
  */
 
 (() => {
@@ -22,16 +22,16 @@
   - 层二功能1 #?<endCluster=true &simpleAnchor=alias
 - attachPanel #?<comment= #YggdrAML
 `;
-    const module = new Object();
-    const behaviours = {
 
+    const behaviours = {
       syncPlay: async() => {
         const aplayerLrc = [
           "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.css",
           "https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js",
           "https://rra.pages.dev/6/aPlr-LrcSync-050.js?exportName=initModule",
         ];
-        const sp = await module.refer(aplayerLrc);
+        const aplay = await behaviours.refer(aplayerLrc);
+        const initModule = aplay[2].initModule;
         initModule();
       },
 
@@ -40,7 +40,8 @@
           "https://gcore.jsdelivr.net/gh/Flyer53/jsPanel4/es6module/jspanel.min.css",
           "https://gcore.jsdelivr.net/gh/Flyer53/jsPanel4/es6module/jspanel.min.js",
         ];
-        const jp = await module.refer(jsPanel4);
+        const panel = await behaviours.refer(jsPanel4);
+        const jsPanel = panel[1].jsPanel;
         jsPanel.create();
       },
 
@@ -52,22 +53,23 @@
       '层一功能2': () => console.log('执行: 层一功能2'),
     };
     try {
-      const rl = await import(core);
-      const mn = await rl.default(libs);
+      const refer = await import(core);
+      const menu = await refer.default(libs);
+      behaviours.refer = refer.default;
       // 创建命令执行器
       const executor = new CommandExecutor({
         globalScope: behaviours,
         debug: true,
       });
-      // window.executor = executor;
+      /* window.executor = executor;
       // 如果需要特殊处理，显式注册
       executor.register('attachPanel', () => {
         console.log('自定义的 attachPanel');
         behaviours.attachPanel();
         // ... 其他逻辑
-      });
+      });*/
       initializeApp ( yggd, executor );
-      module.refer = rl.default;
+
     } catch (error) {
       console.error("jsPanel initialization failed:", error);
       throw error;
@@ -82,7 +84,7 @@
     }
   };
 
-  const initial_Unique = async () => {
+  const initial_Unique = () => {
     if (window.meLoaded) {
       console.warn("Instance already running. Aborting.");
       return;
@@ -91,7 +93,7 @@
     window.meLoaded = true; // 立即标记，防止并发
 
     try {
-      await readyDOM_Adapter();
+      readyDOM_Adapter();
     } catch (error) {
       console.error("Initialization failed:", error);
       window.meLoaded = false; // 失败时释放，允许重试
